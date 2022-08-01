fac6a5af33a70d8f5384ae47c2eb31c0e3d5929d
MaksonGitHub25 <makson94021@gmail.com>
2022-08-01 16:30:50 +0300

    feck off tech from app.js

--- app.js
+++ app.js
@@ -1,218 +1,4 @@
-const tech = [ 
-        [ 
-            {
-                title : 'iPhone 13 pro max' ,
-                category : 'smartphone',
-                price : '$1000',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 6.5,
-                    matrix : 'retina'
-                }
-            },
-            {
-                title : 'iPhone 12' ,
-                category : 'smartphone',
-                price : '$800',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 6.0,
-                    matrix : 'IPS'
-                }
-            },
-            {
-                title : 'samsung s21' ,
-                category : 'smartphone',
-                price : '$700',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 6.2,
-                    matrix : 'IPS'
-                }
-            },
-            {
-                title : 'xiaomi mi note 11 pro' ,
-                category : 'smartphone',
-                price : '$500',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 6.5,
-                    matrix : 'IPS'
-                }
-            }
-        ],
-        [
-            {
-                title : 'samsung TV s8' ,
-                category : 'TV',
-                price : '$600',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 48,
-                    matrix : 'IPS'
-                }
-            }, 
-            {
-                title : 'kivi TV' ,
-                category : 'TV',
-                price : '$400',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 24,
-                    matrix : 'IPS'
-                }
-            },
-            {
-                title : 'samsung TV s7' ,
-                category : 'TV',
-                price : '$500',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 55,
-                    matrix : 'IPS'
-                }
-            }, 
-            {
-                title : 'sony TV' ,
-                category : 'TV',
-                price : '$650',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 32,
-                    matrix : 'IPS'
-                }
-            }
-        ],
-        [
-            {
-                title : 'ps5' ,
-                category : 'gaming',
-                price : '$700',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            }, 
-            {
-                title : 'razer deathadder' ,
-                category : 'gaming',
-                price : '$100',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            },
-            {
-                title : 'ps4' ,
-                category : 'gaming',
-                price : '$400',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            }, 
-            {
-                title : 'razer headphones' ,
-                category : 'gaming',
-                price : '$200',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            }
-        ],
-        [
-            {
-                title : 'apple watch s6' ,
-                category : 'smart-watch',
-                price : '$400',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 1.6,
-                    matrix : 'IPS'
-                }
-            },
-            {
-                title : 'apple watch s7' ,
-                category : 'smart-watch',
-                price : '$500',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 1.8,
-                    matrix : 'IPS'
-                }
-            }, 
-            {
-                title : 'apple watch se' ,
-                category : 'smart-watch',
-                price : '$350',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 1.6,
-                    matrix : 'IPS'
-                }
-            },
-            {
-                title : 'samsung watch' ,
-                category : 'smart-watch',
-                price : '$300',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 1.5,
-                    matrix : 'IPS'
-                }
-            }
-        ],
-        [
-            {
-                title : 'teapot Xiaomi' ,
-                category : 'smart-home',
-                price : '$50',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            },
-            {
-                title : 'apple homepod mini' ,
-                category : 'smart-home',
-                price : '$100',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            }, 
-            {
-                title : 'smart socket' ,
-                category : 'smart-home',
-                price : '$50',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            },
-            {
-                title : 'led line' ,
-                category : 'smart-home',
-                price : '$60',
-                imgURL : 'url_to_image_from_google',
-                description : {
-                    screen : 'withoutMonitor',
-                    matrix : 'withoutMonitor'
-                }
-            }
-        ]
-    ];
-
 const users = [];
-
 
 function menu() {
     const userMenuChoise = prompt(`Выбери функцию:
@@ -229,7 +15,7 @@ f) Выход`);
 function funcChooseHandler(userChoise) {
     switch (userChoise) {
         case 'a':
-            checkConcreateCategory();
+            checkConcreateCategory(tech);
             menu();
             break;
         
@@ -239,7 +25,7 @@ function funcChooseHandler(userChoise) {
             break;
 
         case 'c':
-            sortAllItem();
+            sortAllItem(tech, users);
             menu();
             break;
 
@@ -263,9 +49,68 @@ function funcChooseHandler(userChoise) {
     }
 }
 
-function checkConcreateCategory() {
-    console.log('checkConcreateCategory');
+
+function categoryChooseHandler(choosenCategory, category) {
+    switch (choosenCategory) {
+        case 'a':
+            category = 'smartphone';
+            break;
+        
+        case 'b':
+            category = 'TV';
+            break;
+
+        case 'c':
+            category = 'gaming';
+            break;
+
+        case 'd':
+            category = 'smart-watch';
+            break;
+
+        case 'e':
+            category = 'smart-home';
+            break;
+
+        default:
+            alert('Ты ввел не верное значение категории!');
+            askUserCategory();
+            break;
+    }
+
+    return category;
 }
+
+function askUserCategory() {
+    let userCategory;
+    const choosenCategory = prompt(`Какую категорию вы хотите выбрать?
+a) smartphone
+b) TV
+c) gaming
+d) smart-watch
+e) smart-home`);
+
+    return userCategory = categoryChooseHandler(choosenCategory, userCategory);
+}
+
+function checkConcreateCategory(array) {
+    const category = askUserCategory();
+
+    searchCategoryInArray(array, category);
+}
+
+function searchCategoryInArray(array, categoryForSearch) {
+    array.forEach(function (elem) {
+        _.isArray(elem) ? searchCategoryInArray(elem, categoryForSearch) : checkCategoryInObject(elem, categoryForSearch);
+    });
+}
+
+function checkCategoryInObject(itemObject, categoryForSearch) {
+    if (itemObject.category === categoryForSearch) {
+        console.log(itemObject);
+    }
+}
+
 
 function checkAllItem(mainArray) {
     mainArray.forEach(function (elem) {
@@ -277,10 +122,12 @@ function checkAllItem(mainArray) {
     });
 }
 
-function sortAllItem() {
-    if (/*сделать чек на функцию которая проверяет длину массива и если она < 1, то alert и return*/) {
-        
+function sortAllItem(arrayForSort, users) {
+    if (!checkUsersArrayOnEmpty(users)) {
+        alert('Нету зарегистрированных пользователей!');
+        return;
     }
+
 }
 
 function registerUser(arrayForPush) {
@@ -352,6 +199,12 @@ function checkUsersArrayOnFullness(arrayForCheck) {
     }
 }
 
+function checkUsersArrayOnEmpty(arrayForCheck) {
+    if (arrayForCheck.length === 1) {
+        return true;
+    }
+}
+
 function openBag() {
     console.log('openBag');
 }
@@ -408,7 +261,7 @@ menu();
 //* 2) Категорий должно быть не менее , чем 6 
 //* Товары - не менее 4 шт на категории.
 
-// 3) С помощью промпт дать возможность пользователю выбрать в какую категорию он зайдет .
+//* 3) С помощью промпт дать возможность пользователю выбрать в какую категорию он зайдет .
 // -Затем , предложить критерии к поиску , например : цена , экран , цвет , тд.
 // -Критерии должны формироваться сами , а не хардкодиться ( в это вам помогут ключи (у объекта) )
 
@@ -419,7 +272,7 @@ menu();
 //* 6) Создать объект пользователя , у пользователя есть : имя , фамилия , возраст , логи и пароль.
 
 //* Дать возможность зарегаться юзеру :
-// (Если пользователь не зарегистрирован , он не может использовать фильтры и сортировки)
+//* (Если пользователь не зарегистрирован , он не может использовать фильтры и сортировки)
 //* - Имя не менее 2х букв (не вкл)
 //* - Имел обязательно должен содержать @
 //* - Пароль состоит из чисел , строк и спец. Символов.
@@ -433,10 +286,4 @@ menu();
 // Когда пользователь зарегистрирован , он получает первый кэш на который может приобрести что-либо .
 // Если при совершении покупки у юзера не хватает денег - написать сообщение , что “Нужно больше золота” , а иначе просто списать деньги с юзера .
 
-// Программа работает до тех пор пока юзер не захочет выйти.
-
-// Использовать(подсказка) : while , for , forEach , filter , sort , concat , reduce , reverse .
-
-// () => , FE , FD 
-
-// If …else , switch , ?: 
+//* Программа работает до тех пор пока юзер не захочет выйти.
---
+++ tech.js
@@ -0,0 +1,212 @@
+const tech = [ 
+    [ 
+        {
+            title : 'iPhone 13 pro max' ,
+            category : 'smartphone',
+            price : '$1000',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 6.5,
+                matrix : 'retina'
+            }
+        },
+        {
+            title : 'iPhone 12' ,
+            category : 'smartphone',
+            price : '$800',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 6.0,
+                matrix : 'IPS'
+            }
+        },
+        {
+            title : 'samsung s21' ,
+            category : 'smartphone',
+            price : '$700',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 6.2,
+                matrix : 'IPS'
+            }
+        },
+        {
+            title : 'xiaomi mi note 11 pro' ,
+            category : 'smartphone',
+            price : '$500',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 6.5,
+                matrix : 'IPS'
+            }
+        }
+    ],
+    [
+        {
+            title : 'samsung TV s8' ,
+            category : 'TV',
+            price : '$600',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 48,
+                matrix : 'IPS'
+            }
+        }, 
+        {
+            title : 'kivi TV' ,
+            category : 'TV',
+            price : '$400',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 24,
+                matrix : 'IPS'
+            }
+        },
+        {
+            title : 'samsung TV s7' ,
+            category : 'TV',
+            price : '$500',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 55,
+                matrix : 'IPS'
+            }
+        }, 
+        {
+            title : 'sony TV' ,
+            category : 'TV',
+            price : '$650',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 32,
+                matrix : 'IPS'
+            }
+        }
+    ],
+    [
+        {
+            title : 'ps5' ,
+            category : 'gaming',
+            price : '$700',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        }, 
+        {
+            title : 'razer deathadder' ,
+            category : 'gaming',
+            price : '$100',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        },
+        {
+            title : 'ps4' ,
+            category : 'gaming',
+            price : '$400',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        }, 
+        {
+            title : 'razer headphones' ,
+            category : 'gaming',
+            price : '$200',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        }
+    ],
+    [
+        {
+            title : 'apple watch s6' ,
+            category : 'smart-watch',
+            price : '$400',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 1.6,
+                matrix : 'IPS'
+            }
+        },
+        {
+            title : 'apple watch s7' ,
+            category : 'smart-watch',
+            price : '$500',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 1.8,
+                matrix : 'IPS'
+            }
+        }, 
+        {
+            title : 'apple watch se' ,
+            category : 'smart-watch',
+            price : '$350',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 1.6,
+                matrix : 'IPS'
+            }
+        },
+        {
+            title : 'samsung watch' ,
+            category : 'smart-watch',
+            price : '$300',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 1.5,
+                matrix : 'IPS'
+            }
+        }
+    ],
+    [
+        {
+            title : 'teapot Xiaomi' ,
+            category : 'smart-home',
+            price : '$50',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        },
+        {
+            title : 'apple homepod mini' ,
+            category : 'smart-home',
+            price : '$100',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        }, 
+        {
+            title : 'smart socket' ,
+            category : 'smart-home',
+            price : '$50',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        },
+        {
+            title : 'led line' ,
+            category : 'smart-home',
+            price : '$60',
+            imgURL : 'url_to_image_from_google',
+            description : {
+                screen : 'withoutMonitor',
+                matrix : 'withoutMonitor'
+            }
+        }
+    ]
+];
